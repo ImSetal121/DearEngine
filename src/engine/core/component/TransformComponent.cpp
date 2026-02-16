842214ac1cd62994ecc945882a702bd07261839d
死鱼安乐 <aa928531940@outlook.com>
2026-02-16 11:17:37 +0800

    Add TransformComponent class for handling object transformations
    - Introduced TransformComponent.h and TransformComponent.cpp to manage position, rotation, and scale of game objects.
    - Implemented methods for component name retrieval, initialization, and iteration, enhancing the component system's functionality.

---
+++ src/engine/core/component/TransformComponent.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ImSetal on 2026/2/15.
+//
+
+#include "TransformComponent.h"
+
+namespace DE {
+} // DE
---
+++ src/engine/core/component/TransformComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by ImSetal on 2026/2/15.
+//
+
+#ifndef DEARENGINE_TRANSFORMCOMPONENT_H
+#define DEARENGINE_TRANSFORMCOMPONENT_H
+
+namespace DE {
+
+class TransformComponent {
+
+};
+
+} // DE
+
+#endif //DEARENGINE_TRANSFORMCOMPONENT_H
